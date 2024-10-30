e9c859f6748d760db989b1f191684f1368062273
Pedro Souza <130477280+pedrosouzadevs@users.noreply.github.com>
2024-10-30 11:44:21 -0300

    Update utils.c

--- printf/casa/utils.c
+++ printf/casa/utils.c
@@ -11,3 +11,27 @@ void	ft_putstr_fd(char *s, int fd, int *count)
 	while (*s)
 		ft_putchar_fd(*s++, fd, count);
 }
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (s1[i] == s2[i])
+		{
+			if (s1[i] == '\0')
+				return (0);
+		}
+		else
+		{
+			if ((unsigned char)s1[i] > (unsigned char)s2[i])
+				return (1);
+			else
+				return (-1);
+		}
+		i++;
+	}
+	return (0);
+}
