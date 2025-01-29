6d92daf28aa59c3636567529d04c1f8941b77996
Pedro-hm <pedro-hm@c3r13p4.42sp.org.br>
2025-01-29 17:13:12 -0300

    oi

--- so_long/src/render_map.c
+++ so_long/src/render_map.c
@@ -6,7 +6,7 @@
 /*   By: pedro-hm <pedro-hm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 14:02:56 by pedro-hm          #+#    #+#             */
-/*   Updated: 2025/01/27 15:55:07 by pedro-hm         ###   ########.fr       */
+/*   Updated: 2025/01/29 17:12:24 by pedro-hm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,33 +16,29 @@ void init_images(t_game *game)
 {
 	xpm_t* xpm;
 
-    // game->map.wall_img = mlx_xpm_file_to_image(game->mlx, "assets/wall.xpm", &img_width, &img_height);
 	xpm = mlx_load_xpm42("assets/wall.xpm42");
 	game->map.wall_img = mlx_texture_to_image(game->mlx, &xpm->texture);
 	mlx_delete_xpm42(xpm);
 
-    // game->map.collectible_img = mlx_xpm_file_to_image(game->mlx, "assets/floor.xpm", &img_width, &img_height);
 	xpm = mlx_load_xpm42("assets/floor.xpm42");
 	game->map.collectible_img = mlx_texture_to_image(game->mlx, &xpm->texture);
 	mlx_delete_xpm42(xpm);
 
-    // game->map.exit_img = mlx_xpm_file_to_image(game->mlx, "assets/floor.xpm", &img_width, &img_height);
 	xpm = mlx_load_xpm42("assets/wall.xpm42");
 	game->map.exit_img = mlx_texture_to_image(game->mlx, &xpm->texture);
 	mlx_delete_xpm42(xpm);
 
-    // game->map.floor_img = mlx_xpm_file_to_image(game->mlx, "assets/floor.xpm", &img_width, &img_height);
+	xpm = mlx_load_xpm42("assets/wall.xpm42");
+	game->map.platform_img = mlx_texture_to_image(game->mlx, &xpm->texture);
+	mlx_delete_xpm42(xpm);
+
 	xpm = mlx_load_xpm42("assets/floor.xpm42");
 	game->map.floor_img = mlx_texture_to_image(game->mlx, &xpm->texture);
 	mlx_delete_xpm42(xpm);
 
-    // game->player.image = mlx_xpm_file_to_image(game->mlx, "assets/sf1_b.xpm", &img_width, &img_height);
 	xpm = mlx_load_xpm42("assets/sf1_b.xpm42");
-	if (!xpm) {
-    write(2, "Erro ao carregar textura wall.xpm42\n", 36);
-    exit(EXIT_FAILURE);
-	}
 	game->player.image= mlx_texture_to_image(game->mlx, &xpm->texture);
+	mlx_resize_image(game->player.image, TILE_SIZE, TILE_SIZE);
 	mlx_delete_xpm42(xpm);
 }
 void render_map(t_game *game) {
@@ -56,15 +52,20 @@ void render_map(t_game *game) {
 		while (game->map.map[y][x])
 		{ // Percorre os caracteres da linha
 			if (game->map.map[y][x] == '1')
-				mlx_image_to_window(game->mlx, game->map.wall_img, x * TILE_SIZE, y * TILE_SIZE);
+				if (is_edge_wall(game))
+					mlx_image_to_window(game->mlx, game->map.platform_img, x * TILE_SIZE, y * TILE_SIZE);
+				else
+					mlx_image_to_window(game->mlx, game->map.wall_img, x * TILE_SIZE, y * TILE_SIZE);
 			else
+			{
 				mlx_image_to_window(game->mlx, game->map.floor_img, x * TILE_SIZE, y * TILE_SIZE);
-			if (game->map.map[y][x] == 'E')
-				mlx_image_to_window(game->mlx, game->map.exit_img, x * TILE_SIZE, y * TILE_SIZE);
-			else if (game->map.map[y][x] == 'C')
-				mlx_image_to_window(game->mlx, game->map.collectible_img, x * TILE_SIZE, y * TILE_SIZE);
-			else if (game->map.map[y][x] == 'T')
-				mlx_image_to_window(game->mlx, game->map.enemy_img, x * TILE_SIZE, y * TILE_SIZE);
+				if (game->map.map[y][x] == 'E')
+					mlx_image_to_window(game->mlx, game->map.exit_img, x * TILE_SIZE, y * TILE_SIZE);
+				else if (game->map.map[y][x] == 'C')
+					mlx_image_to_window(game->mlx, game->map.collectible_img, x * TILE_SIZE, y * TILE_SIZE);
+				else if (game->map.map[y][x] == 'T')
+					mlx_image_to_window(game->mlx, game->map.enemy_img, x * TILE_SIZE, y * TILE_SIZE);
+			}
 			x++;
 		}
 		y++;
@@ -72,6 +73,11 @@ void render_map(t_game *game) {
 }
 
 void	verify_wall(t_game *game)
+{
+	if (is_edge_wall(game))
+		error_wall();
+}
+int	is_edge_wall(t_game *game)
 {
 	int y;
 	int x;
@@ -85,15 +91,16 @@ void	verify_wall(t_game *game)
 			if (y == 0 || y == game->map.height - 1)
 				if (x >= 0 && x <= game->map.width - 1)
 					if (game->map.map[y][x] != '1')
-					error_wall();
+					return (1);
 			if (y > 0 && y < (game->map.height - 1))
 				if (x == 0 || x == game->map.width - 1)
 					if (game->map.map[y][x] != '1')
-					error_wall();
+					return (1);
 			x++;
 		}
 		y++;
 	}
+	return (0);
 }
 
 void	verify_map(t_game *game)
@@ -122,6 +129,7 @@ void	verify_map(t_game *game)
 		printf("Error\nMap must have no more than one exit and one player\n");
 		exit(EXIT_FAILURE);
 	}
+	is_map_retangle(game);
 }
 void	find_player_position(t_game *game)
 {
@@ -148,4 +156,13 @@ void	error_none_player_exit_colec(void)
 {
 	printf("Error\nMap must have at least one exit, one collectible and one player\n");
 	exit(EXIT_FAILURE);
+}
+
+void	is_map_retangle(t_game *game)
+{
+	if (game->map.width == game->map.height)
+	{
+		printf("Error\nMap must be a rectangle\n");
+		exit(EXIT_FAILURE);
+	}
 }
